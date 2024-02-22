c768f956fc23f2d7aad678d802f8659e368e0787
jvpigozzo <jvpigozzo@gmail.com>
2024-02-22 19:22:55 -0300

    config vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-chartjs-2']
+    }
+  }
+});
